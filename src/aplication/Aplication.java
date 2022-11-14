1a4f6618d930a4259f84b48f06c3c4cd1cee7e13
Edmundo <edmundojr32@gmail.com>
2022-11-14 10:21:19 -0300

    Tratamento de exceções

--- src/aplication/Aplication.java
+++ src/aplication/Aplication.java
@@ -1,7 +1,13 @@
 package aplication;
 
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessMatch;
+
 public class Aplication {
     public static void main(String[] args) {
 
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
